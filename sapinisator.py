9ebb16d6f8dbd7f97a539927cb9bbc51b91946ba
antoinemoreau <antoine.moreau.v@orange.fr>
2017-12-20 17:37:24 +0100

    a

--- sapinisator.py
+++ sapinisator.py
@@ -1,4 +1,26 @@
 #! /usr/bin/env python3
 
+import struct
+
+class Point:
+    def __init__(self, x, y ,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Triangle:
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+def read_stl(filename):
+    with open(filename, "rb") as f:
+        head = f.read(80)
+        nbtriangles = f.read(4)
+        print(nbtriangles)
+        nbtriangles = struct.unpack("<I", nbtriangles)
+        print(nbtriangles)
+       
 def main():
-    pass
+    read_stl("Schichtschwein_ohne_Logo.stl")
